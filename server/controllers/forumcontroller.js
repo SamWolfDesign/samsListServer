095aee6f607a0955be378bec56e9f4d7b50e0af9
Sam Wolf <samwolf37@gmail.com>
2020-12-18 12:09:36 -0500

    validate session worked in

--- server/controllers/forumcontroller.js
+++ server/controllers/forumcontroller.js
@@ -1,14 +1,10 @@
 let express = require('express');
 let router = express.Router();
+let validateSession = require('../middleware/validate-session');
 
-
-router.get('/practice', function(req, res)
+router.get('/practice', validateSession, function(req, res)
 {
     res.send('Hey!! This is a fuckin practice route!')
 })
 
-router.get('/about', function(req, res)
-{
-    res.send('This is the goddamn about route')
-})
 module.exports = router
---
+++ server/middleware/validate-session.js
@@ -0,0 +1,35 @@
+const jwt = require('jsonwebtoken');
+const db = require('../db');
+const User = require('../models/user');
+
+const validateSession = (req, res, next) => {
+    const token = req.headers.authorization;
+
+    if(!token) {
+        return res.status(403).send({ auth: false, message: "No tokey 4 u :("})
+    } else {
+        jwt.verify(token, process.env.JWT_SECRET, (err, decodeToken) => {
+
+            if(!err && decodeToken) {
+                User.findOne({
+                    where: {
+                        id: decodeToken.id
+                    }
+                })
+                .then(user => {
+
+                    if(!user) throw err;
+
+                    req.user = user;
+                    return next();
+                })
+                .catch(err => next(err));
+            } else {
+                req.errors = err;
+                return res.status(500).send('Not Authorized >:)');
+            }
+        });
+    }
+};
+
+module.exports = validateSession;
