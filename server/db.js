7e2b37949ad91efa3675c400ab6de3c2b09d3dfa
Sam Wolf <samwolf37@gmail.com>
2021-01-14 21:22:13 -0500

    doing some thangs with the logs

--- server/db.js
+++ server/db.js
@@ -1,14 +1,14 @@
 const { Sequelize } = require('sequelize');
-// const sequelize = new Sequelize(process.env.DATABASE_URL, {
-//     dialect: "postgres"
-// });
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: "postgres"
+});
 
-const sequelize = new Sequelize(
-    process.env.DATABASE_URL ||
-    `postgresql://postgres:${encodeURIComponent(process.env.PASS)}@localhost/sam-list2`,
-    {
-        dialect: 'postgres',
-    }
-)
+// const sequelize = new Sequelize(
+//     process.env.DATABASE_URL ||
+//     `postgresql://postgres:${encodeURIComponent(process.env.PASS)}@localhost/sam-list2`,
+//     {
+//         dialect: 'postgres',
+//     }
+// )
 
-module.exports = sequelize;
+module.exports = db;
